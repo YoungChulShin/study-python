d0b3dcb0e663a61d6f30aff3c3590bfa744ae489
YoungChulShin-Russel <youngchul.shin@meshkorea.net>
2022-01-15 17:26:14 +0900

    설치 스크립트 추가

---
+++ python_install/install_python.sh
@@ -0,0 +1,30 @@
+# get path
+REPO_ROOT_PATH=$(git rev-parse --show-toplevel)
+
+# install pyevnt
+PYTHON_VERSION="3.8.6"
+VIRTUAL_ENV="the-red-dev"
+PYENV_DIR=~/.pyenv
+
+install_python_version=$(pyenv versions | grep $PYTHON_VERSION)
+if [ -z "$install_python_version"]; then # -z는 길이가 인 경우
+  echo "   1-1) INSTALLING PYTHON $PYTHON_VERSION"
+  pyenv install $PYTHON_VERSION
+  pyenv shell $PYTHON_VERSION
+else 
+  echo "   1-1) INSTALLING PYTHON $PYTHON_VERSION (DONE)"
+fi
+
+installenv=$(pyenv verions | grep $VIRTUAL_ENV)
+if [ -z "$installenv"]; then
+  ehco "  1-2) INSTALLING VIRTUALENV $VIRTUAL_ENV"
+  pyenv virtualenv $PYTHON_VERSION $VIRTUAL_ENV;
+else
+  echo "  1-2) INSTALLING VIRTUALENV $VIRTUAL_ENV (DONE)"
+fi
+
+echo "   2) use $VIRTUAL_ENV"
+
+pyenv local $VIRTUAL_ENV
+pyenv rehash
+pip install --upgrade pip
